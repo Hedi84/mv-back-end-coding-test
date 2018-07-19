ca24002cdddb177785475c8896950f2c8d42b8d3
Hedi van Meurs <alisoun@gmail.com>
2018-07-19 18:56:30 +0100

    first front-end

--- test/influencerlist/app/controllers/pages_controller.rb
+++ test/influencerlist/app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
 
   def home
+
   @user = current_user
   @influencers = Influencer.all
   @starred_influencers = @user.starred_influencers
+
   end
 
 end
