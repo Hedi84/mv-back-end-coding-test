5e223b1e966a16279b45ddbb081ca3e25a31c09f
Hedi van Meurs <alisoun@gmail.com>
2018-07-20 13:52:21 +0100

    installed gems, added logic for better suggestions

--- test/influencerlist/app/controllers/pages_controller.rb
+++ test/influencerlist/app/controllers/pages_controller.rb
@@ -6,6 +6,10 @@ class PagesController < ApplicationController
   @influencers = Influencer.all
   @starred_influencers = @user.starred_influencers
 
+  ids = []
+    @starred_influencers.each do |starred_influencer|
+     ids << starred_influencer.influencer_id
+    end
+    @ids = ids
   end
-
 end
