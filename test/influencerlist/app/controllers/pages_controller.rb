e65d446ff79b402e6fa93470613b2c2298a224db
Hedi van Meurs <alisoun@gmail.com>
2018-07-21 23:53:42 +0100

    filter works half

--- test/influencerlist/app/controllers/pages_controller.rb
+++ test/influencerlist/app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
 
-helper_method :sort_by_params, :sort_direction
+
 
   def home
+
+
+    @hash = {
+      "statistics engagement desc" => "filter"
+    }
     @user = current_user
     @influencers = Influencer.all
     @starred_influencers = @user.starred_influencers
@@ -19,14 +24,24 @@ helper_method :sort_by_params, :sort_direction
       @influencers_starred = influencers_starred
     end
 
+     @user_options = [["Sort by", ""],["Engagement (Low-High)", "statistics engagement desc."], ["Engagement (High-Low)", "statistics engagement asc."], ["Followers (Low-High)", "followers desc."], ["Followers (High-Low)", "followers asc."], ["Full Name", "name"]]
+
     filter = params[:sort]
 
     if filter.nil?
       @starred_influencer
     elsif filter == "statistics engagement desc."
-      @influencers_starred = influencers_starred.sort! { |a,b| a.statistics_engagement <=> b.sort_by_params }
+      @influencers_starred = influencers_starred.sort! { |a,b| a.statistics_engagement <=> b.statistics_engagement}
     elsif filter == "statistics engagement asc."
-      @influencers_starred = influencers_starred.sort! { |a| a.statistics_engagement }
+      @influencers_starred = influencers_starred.compact.sort_by! { |a, b| a.statistics_engagement}
+    elsif filter == "followers desc."
+      influencers_starred.sort! { |a,b| a.statistics_followers <=> b.statistics_followers}
+    elsif filter == "followers asc."
+      influencers_starred.sort! { |a| a.statistics_followers }
+    elsif filter == "name"
+      influencers_starred.sort_by! { |a| a.influencer_full_name.downcase }
+
+
 
     end
   end
