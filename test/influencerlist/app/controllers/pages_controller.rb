805181467e73bd68b154e61b2d195b0f45d38612
Hedi van Meurs <alisoun@gmail.com>
2018-07-22 00:39:45 +0100

    filter works, added extras. flexbox for drodown

--- test/influencerlist/app/controllers/pages_controller.rb
+++ test/influencerlist/app/controllers/pages_controller.rb
@@ -24,7 +24,7 @@ class PagesController < ApplicationController
       @influencers_starred = influencers_starred
     end
 
-     @user_options = [["Sort by", ""],["Engagement (Low-High)", "statistics engagement desc."], ["Engagement (High-Low)", "statistics engagement asc."], ["Followers (Low-High)", "followers desc."], ["Followers (High-Low)", "followers asc."], ["Full Name", "name"]]
+     @user_options = [["Sort by", ""],["Engagement (Low-High)", "statistics engagement desc."], ["Engagement (High-Low)", "statistics engagement asc."], ["Followers (Low-High)", "followers desc."], ["Followers (High-Low)", "followers asc."], ["Full Name", "name"], ["User Name", "username"]]
 
     filter = params[:sort]
 
@@ -33,16 +33,15 @@ class PagesController < ApplicationController
     elsif filter == "statistics engagement desc."
       @influencers_starred = influencers_starred.sort! { |a,b| a.statistics_engagement <=> b.statistics_engagement}
     elsif filter == "statistics engagement asc."
-      @influencers_starred = influencers_starred.compact.sort_by! { |a, b| a.statistics_engagement}
+      @influencers_starred = influencers_starred.sort! { |a,b| b.statistics_engagement <=> a.statistics_engagement}
     elsif filter == "followers desc."
       influencers_starred.sort! { |a,b| a.statistics_followers <=> b.statistics_followers}
     elsif filter == "followers asc."
-      influencers_starred.sort! { |a| a.statistics_followers }
+      influencers_starred.sort! { |a,b| b.statistics_followers <=> a.statistics_followers}
     elsif filter == "name"
       influencers_starred.sort_by! { |a| a.influencer_full_name.downcase }
-
-
-
+    elsif filter == "username"
+      influencers_starred.sort_by! { |a| a.influencer_instagram_username.downcase }
     end
   end
 
